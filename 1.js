fa69eb4e5b81d70d8f2e4c24284736cebcbe7253
KeyaAkter <sanhatanvin12@gmail.com>
2023-01-24 23:35:46 +0600

    exam revised

--- 1.js
+++ 1.js
@@ -3,16 +3,24 @@ const b = [2, 3, 5, 6, 10, 12];
 
 const c = [1, 4, 8, 9, 2, 3, 10, 12]; // coherence of a & b
 
-function linearSearch(arr, target) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === target) {
-      return i;
+function quickSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  let pointer = arr[0];
+  const left = [];
+  const right = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > pointer) {
+      right.push(arr[i]);
+    } else {
+      left.push(arr[i]);
     }
   }
 
-  return -1;
+  return [...quickSort(left), pointer, ...quickSort(right)];
 }
 
-console.log(linearSearch(c, 9));
+console.log(quickSort(c));
 
-// time complexity & space complexity: O(n), because here time and space depends on input size. If input size increases, time & space complexity will also increase and vice-versa.
+// time complexity : O(n^2) - quadreatic & space complexity: O(n) - linear, because here space depends on input size. But during one cycle, recursively same function called twice.Because of this reason time complexity becomes quadratic.
